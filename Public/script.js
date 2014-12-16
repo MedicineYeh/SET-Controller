3104771b7df42555255ee1fa28ab488717c5bdb8
MedicineYeh <medicinehy@gmail.com>
2014-12-16 23:55:45 +0800

    first prototype of set controller

--- Public/script.js
+++ Public/script.js
@@ -1,70 +1,94 @@
 angular.module('dashboard', ['ngMaterial'])
 .controller('AppCtrl', function($scope) {
-    $scope.data = {};
-    $scope.data.cb1 = true;
-    $scope.data.cb2 = false;
-
-    $scope.data = {
-        group1 : 'Banana',
-        group2 : '3'
-    };
-
-    $scope.radioData = [
-    { label: '1', value: '1' },
-    { label: '2', value: '2' },
-    { label: '3', value: '3' }
-    ];
-
-    $scope.addItem = function() {
-        var r = Math.ceil(Math.random() * 1000);
-        $scope.radioData.push({ label: r, value: r });
-    };
-
-    $scope.removeItem = function() {
-        $scope.radioData.pop();
-    };
-
+    //VMs
     var tabs = [
-        { title: 'One', content: "Tabs will become paginated if there isn't enough room for them."},
-        { title: 'Two', content: "You can swipe left and right on a mobile device to change tabs."},
-        { title: 'Three', content: "You can bind the selected tab via the selected attribute on the md-tabs element."},
-        { title: 'Four', content: "If you set the selected tab binding to -1, it will leave no tab selected."},
-        { title: 'Five', content: "If you remove a tab, it will try to select a new one."},
-        { title: 'Six', content: "There's an ink bar that follows the selected tab, you can turn it off if you want."},
-        { title: 'Seven', content: "If you set ng-disabled on a tab, it becomes unselectable. If the currently selected tab becomes disabled, it will try to select the next tab."},
-        { title: 'Eight', content: "If you look at the source, you're using tabs to look at a demo for tabs. Recursion!"},
-        { title: 'Nine', content: "If you set md-theme=\"green\" on the md-tabs element, you'll get green tabs."},
-        { title: 'Ten', content: "If you're still reading this, you should just go check out the API docs for tabs!"}
+        { title: 'One', content: "Tabs will become paginated if there isn't enough room for them."}
     ];
 
     $scope.tabs = tabs;
-    $scope.selectedIndex = 2;
-
-    $scope.announceSelected = announceSelected;
-    $scope.announceDeselected = announceDeselected;
-
-    $scope.addTab = function (title, view) {
-        view = view || title + " Content View";
+    $scope.selectedIndex = 0;
+    $scope.addTab = function (title) {
+        view = title + " Content View";
         tabs.push({ title: title, content: view, disabled: false});
         $scope.tTitle = "";
         $scope.tContent = "";
     };
 
-    $scope.removeTab = function (tab) {
-        for (var j = 0; j < tabs.length; j++) {
-            if (tab.title == tabs[j].title) {
-                $scope.tabs.splice(j, 1);
-                break;
-            }
-        }
+    $scope.removeTab = function (index) {
+        $scope.tabs.splice(index, 1);
     };
 
-    function announceDeselected(tab) {
-        $scope.farewell = 'Goodbye ' + tab.title + '!';
+    //Process
+    $scope.traced_processes = [];
+
+    $scope.add_process = function (name) {
+        if (name !== undefined && name !== "") {
+            console.log(name);
+            $scope.traced_processes.push({name: name});
+        }
+        $scope.process_name = "";
+    };
+
+    $scope.remove_process = function (index) {
+        $scope.traced_processes.splice(index, 1);
+    };
+
+    //Timing Model
+    $scope.model = {};
+
+    $scope.model.timing = [
+    {name: 'Basic', value: true},
+    {name: 'Pipeline', value: false},
+    {name: 'I/D Cache', value: false},
+    {name: 'Branch Prediction', value: false}
+    ];
+
+    $scope.model.events = [
+    {name: 'IRQ', value: true},
+    {name: 'Kernel', value: true},
+    {name: 'System Call', value: true},
+    {name: 'Library', value: true},
+    {name: 'User C/C++', value: false},
+    {name: 'Dalvik', value: true}
+    ];
+
+
+    //Websocket
+    var ws = new WebSocket('ws://127.0.0.1:8080');
+    ws.onopen = function()
+    {
+        // Web Socket is connected, send data using send()
+        ws.send("Message to send");
+        tabs[$scope.selectedIndex].content = "ws: Send a message";
+        $scope.$apply();
+    };
+
+    ws.onmessage = function (evt) 
+    { 
+        var received_msg = evt.data;
+        tabs[$scope.selectedIndex].content = "ws: Receive " + received_msg;
+        $scope.$apply();
+    };
+
+    ws.onclose = function()
+    { 
+        // websocket is closed.
+        tabs[$scope.selectedIndex].content = "ws: Connection is closed...";
+    };
+
+    function replacer(key, value) {
+        if (key === "$$hashKey") {
+            return undefined;
+        }
+        return value;
     }
 
-    function announceSelected(tab) {
-        $scope.greeting = 'Hello ' + tab.title + '!';
+    $scope.start_trace = function () {
+        var data = "{\"type\": \"start_trace\", \"content\": ";
+        data += JSON.stringify($scope.model, replacer);
+        data += "}";
+
+        ws.send(data);
     }
 });
 
--- app.js
+++ app.js
@@ -2,12 +2,12 @@ var express = require( 'express' );
 var http    = require( 'http' );
 var path    = require( 'path' );
 var app     = express();
-var WebSocketServer = require('ws').Server
-  , wss = new WebSocketServer({port: 8080});
+var WebSocketServer = require('ws').Server;
 var cnt = 0;
  
 // all environments
 app.set( 'port', process.env.PORT || 3000 );
+app.set( 'websocket_port', process.env.PORT || 8080 );
 app.use( express.favicon());
 app.use( express.logger( 'dev' ));
 app.use( express.json());
@@ -15,17 +15,15 @@ app.use( express.urlencoded());
 app.use( express.methodOverride());
 app.use( app.router );
 app.use( express.static( path.join( __dirname, 'Public' )));
- 
+
+
+var wss = new WebSocketServer({port: app.get( 'websocket_port' )});
+
 // development only
 if ( 'development' == app.get( 'env' )) {
     app.use( express.errorHandler());
 }
  
-app.get( '/', function(req, res) {
-    console.log('path :', req.url);
-    response.sendfile(req.url);
-} );
-
 wss.on('connection', function(ws) {
     ws.on('message', function(message) {
         console.log('received: %s', message);
@@ -40,5 +38,6 @@ wss.on('connection', function(ws) {
 });
 
 http.createServer( app ).listen( app.get( 'port' ), function(){
-  console.log( 'Express server listening on port ' + app.get( 'port' ));
+    console.log( 'Express server listening on port ' + app.get( 'port' ));
+    console.log( 'Websocket server listening on port ' + app.get( 'websocket_port' ));
 } );
